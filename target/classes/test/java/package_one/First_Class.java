8a3b19df49b2bd10e32fac810e01fa7f0d988cb3
Palash <1988palashc@gmail.com>
2020-02-27 10:57:34 +0530

    This is commit from D workspace

--- target/classes/test/java/package_one/First_Class.java
+++ target/classes/test/java/package_one/First_Class.java
@@ -9,5 +9,9 @@ public class First_Class {
 		System.out.println("This is a Demo 1");
 		System.out.println("This is a Demo 2");
 	}
+	
+	public void get_sele(){
+		System.out.println("This is a demo Git 3");
+	}
 
 }
